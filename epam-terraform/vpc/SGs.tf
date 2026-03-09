df2576ba440d55cfd01dd2070a00027233b87491
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-09 12:50:13 +0500

    Asjust for the absible task

--- epam-terraform/vpc/SGs.tf
+++ epam-terraform/vpc/SGs.tf
@@ -1,35 +1,12 @@
-# Security Group for the Public Classic Load Balancer
-resource "aws_security_group" "lb_sg" {
-  name        = "classic-lb-sg"
-  description = "Allow HTTP traffic from the internet"
-  vpc_id      = aws_vpc.vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Security Group for Nginx Instances (Not internet facing)
-resource "aws_security_group" "nginx_sg" {
-  name        = "nginx-instance-sg"
-  description = "Allow traffic ONLY from the Load Balancer"
+resource "aws_security_group" "instance_sg" {
+  name        = "instance-sg"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [aws_security_group.lb_sg.id]
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -47,12 +24,7 @@ resource "aws_security_group" "nginx_sg" {
   }
 }
 
-output "nginx_sg_id" {
-  value = aws_security_group.nginx_sg.id
-  sensitive = true
-}
-
-output "lb_sg_id" {
-  value = aws_security_group.lb_sg.id
+output "sg_id" {
+  value = aws_security_group.instance_sg.id
   sensitive = true
 }
--- epam-terraform/vpc/locals.tf
+++ epam-terraform/vpc/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  region             = "eu-south-1"
-  project_name       = "epam-terraform"
+  region             = "eu-central-1"
+  project_name       = "epam-ansible"
   vpc_cidr_block     = "10.0.0.0/16"
   subnet_public_cidr = "10.0.1.0/24"
 }
