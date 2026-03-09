a88371be71fb6914de51b321c41fad5c3acbdd53
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-09 12:51:56 +0500

    now the VPC's state file is used

--- epam-terraform/ec2/data.tf
+++ epam-terraform/ec2/data.tf
@@ -2,9 +2,9 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket = "aws-terrafrom-states"
-    key    = "epam-terraform-practical-task/VPC/terraform.tfstate"
-    region = "eu-south-1"
-    profile = "default"
+    bucket       = "aws-ansible-states"
+    key          = "epam-ansible-practical-task/VPC/terraform.tfstate"
+    region       = "eu-central-1"
+    profile      = "second"
   }
 }
