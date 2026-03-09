be6d9ae6c0414bd3aaf027e8c6a420e748f026d3
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-09 13:03:17 +0500

    tf fmt

--- epam-terraform/ec2/data.tf
+++ epam-terraform/ec2/data.tf
@@ -2,9 +2,9 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket       = "aws-ansible-states"
-    key          = "epam-ansible-practical-task/VPC/terraform.tfstate"
-    region       = "eu-central-1"
-    profile      = "second"
+    bucket  = "aws-ansible-states"
+    key     = "epam-ansible-practical-task/VPC/terraform.tfstate"
+    region  = "eu-central-1"
+    profile = "second"
   }
 }
